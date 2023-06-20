1745a70d8e1884d3c686c8de6b6ec44d8b973b90
alwaysJAGADEESH <jagadeeshmaturi8898@gmail.com>
2023-06-20 10:34:59 +0000

    browserHistory

--- src/component/SearchItem/index.js
+++ src/component/SearchItem/index.js
@@ -1,3 +1,5 @@
+import './index.css'
+
 const SearchItem = props => {
   const {searchDetails, deleteTodo} = props
   const {id, timeAccessed, logoUrl, title, domainUrl} = searchDetails
@@ -8,12 +10,21 @@ const SearchItem = props => {
 
   return (
     <li className="historyItem">
-      <h1 className="time">{timeAccessed}</h1>
-      <p className="logoUrl">{logoUrl}</p>
-      <p className="title">{title}</p>
-      <p className="domainUrl">{domainUrl}</p>
+      <div className="time-container">
+        <p className="time">{timeAccessed}</p>
+      </div>
+      <div className="address-container">
+        <img src={logoUrl} alt="domain logo" className="logoUrl" />
+        <p className="title">{title}</p>
+        <p className="domainUrl">{domainUrl}</p>
+      </div>
       <div className="delete-button-container">
-        <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+        <button
+          type="button"
+          className="delete-btn"
+          data-testid="delete"
+          onClick={onDeleteTodo}
+        >
           <img
             src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
             alt="delete"
--- src/component/SearchMain/index.js
+++ src/component/SearchMain/index.js
@@ -1,13 +1,86 @@
 import {Component} from 'react'
 import SearchItem from '../SearchItem'
+import './index.css'
+
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
 
 class SearchMain extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      searchInput: '',
-      DeleteTodo: props.initialHistoryList,
-    }
+  state = {
+    searchInput: '',
+    DeleteTodo: initialHistoryList,
   }
 
   deleteTodo = id => {
@@ -15,10 +88,7 @@ class SearchMain extends Component {
     const updatedDeleteList = DeleteTodo.filter(
       eachHistory => eachHistory.id !== id,
     )
-
-    this.setState({
-      DeleteTodo: updatedDeleteList,
-    })
+    this.setState({DeleteTodo: updatedDeleteList})
   }
 
   onChangeSearchInput = event => {
@@ -26,7 +96,8 @@ class SearchMain extends Component {
   }
 
   render() {
-    const {searchInput, DeleteTodo} = this.state
+    const {searchInput} = this.state
+    const {DeleteTodo} = this.state
     const searchResults = DeleteTodo.filter(eachHistory =>
       eachHistory.title.toLowerCase().includes(searchInput.toLowerCase()),
     )
@@ -35,12 +106,17 @@ class SearchMain extends Component {
       <div className="app-container">
         <div className="search-container">
           <div className="heading-container">
-            <h1 className="heading">History</h1>
+            <h1 className="heading">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/history-website-logo-img.png"
+                alt="app logo"
+              />
+            </h1>
           </div>
           <div className="searchbar">
             <img
-              src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-              alt="search icon"
+              src="https://assets.ccbp.in/frontend/react-js/search-img.png"
+              alt="search"
               className="search-icon"
             />
             <input
@@ -52,15 +128,25 @@ class SearchMain extends Component {
             />
           </div>
         </div>
-        <ul className="initialHistory-list">
-          {searchResults.map(eachHistory => (
-            <SearchItem
-              key={eachHistory.id}
-              searchDetails={eachHistory}
-              deleteTodo={this.deleteTodo}
-            />
-          ))}
-        </ul>
+        <div>
+          {searchResults.length < 1 ? (
+            <div>
+              <p>There is no history to show</p>
+            </div>
+          ) : (
+            <div className="background-container">
+              <ul className="initialHistory-list">
+                {searchResults.map(eachHistory => (
+                  <SearchItem
+                    key={eachHistory.id}
+                    searchDetails={eachHistory}
+                    deleteTodo={this.deleteTodo}
+                  />
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
